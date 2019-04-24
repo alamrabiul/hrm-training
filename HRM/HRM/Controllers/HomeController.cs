d9d714b57076d5c3743cfcccf644ab905ad1663d
rabiulmiu <likhon.miu31@gmail.com>
2019-04-24 20:34:02 +0600

    Index page Summary

--- HRM/HRM/Controllers/HomeController.cs
+++ HRM/HRM/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HRM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,25 @@ namespace HRM.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
+            
+          
+            var nmbrofDept = db.Depts.Count();
+            var nmbrofDivision = db.Divisions.Count();
+            var nmbrofSection = db.Sections.Count();
+            var nmbrofDesignation = db.Designation.Count();
+            var nmbrofEmployee = db.Employee.Count();
+
+
+
+            ViewBag.nmbrofDept = db.Depts.Count();
+            ViewBag.nmbrofDivision = db.Divisions.Count();
+            ViewBag.nmbrofSection = db.Sections.Count();
+            ViewBag.nmbrofDesignation = db.Designation.Count();
+            ViewBag.nmbrofEmployee = db.Employee.Count();
             return View();
         }
 
